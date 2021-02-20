8fabb1f3dcc611cd0ed9c8ca0849b451d3b39492
ali <alila3883@gmail.com>
2021-02-20 23:16:09 +0300

    first push

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,32 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>City</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Ali</td>
+            <td>Jeddah</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Saad</td>
+            <td>Abha</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Ahmed</td>
+            <td>Riyadh</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {}
+</script>
