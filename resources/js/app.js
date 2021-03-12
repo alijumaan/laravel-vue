b4834b8876f4fced6de3c2ee61ea5b4dad2c4e9d
ali <alila3883@gmail.com>
2021-03-13 00:49:38 +0300

    install vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import vueRouter from "vue-router"
 import routes from './routes/index.js'
 import App from './components/App.vue'
+import store from './store'
 import swal from 'sweetalert';
 
 Vue.use(vueRouter)
@@ -15,5 +16,7 @@ Vue.component('pagination', require('laravel-vue-pagination'));
 const app = new Vue({
     el: '#app',
     components: { App },
-    router: new vueRouter(routes)
+    router: new vueRouter(routes),
+    store,
+    swal
 });
